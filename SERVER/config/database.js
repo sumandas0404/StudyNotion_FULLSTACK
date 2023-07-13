aa32ac78fe8698c289ee41f1ec0fc6b3a4dbf634
sumandas0404 <sumondas0404@gmail.com>
2023-07-13 12:11:45 +0530

    databse connection Established

--- SERVER/config/database.js
+++ SERVER/config/database.js
@@ -1,15 +1,15 @@
-// const mongoose = require("mongoose");
-// require("dontenv").config();
+const mongoose = require("mongoose");
+require("dontenv").config();
 
-// exports.connect = () =>{
-//     mongoose.connect(process.env.MONGODB_URL, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology:true,
-//     })
-//     .then( ()=> console.log("DB Connected Successfully"))
-//     .catch( (error) => {
-//         console.log("DB Connection Failed");
-//         console.error(error);
-//         process.exit(1);
-//     })
-// };
+exports.connect = () =>{
+    mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology:true,
+    })
+    .then( ()=> console.log("DB Connected Successfully"))
+    .catch( (error) => {
+        console.log("DB Connection Failed");
+        console.error(error);
+        process.exit(1);
+    })
+};
